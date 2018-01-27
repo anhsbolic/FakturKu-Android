8287cbba15f550b8275f884814551aa6ff01bfad
anhsbolic <anhsbolic@gmail.com>
2018-01-28 06:43:22 +0700

    Init Package Folder (with MVP arch)

--- app/src/main/java/com/fakturku/aplikasi/view/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/fakturku/aplikasi/view/activity/SplashScreenActivity.kt
@@ -1,7 +1,8 @@
-package com.fakturku.aplikasi
+package com.fakturku.aplikasi.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.fakturku.aplikasi.R
 
 class SplashScreenActivity : AppCompatActivity() {
 
