a1cb4c7567975d02c9c214e87312ead9342f39be
unknown <anhsbolic@gmail.com>
2018-03-29 16:19:46 +0700

    [ProductDetails] load&set product data

--- app/src/main/java/com/fakturku/aplikasi/ui/activity/productDetails/ProductDetailsActivity.kt
+++ app/src/main/java/com/fakturku/aplikasi/ui/activity/productDetails/ProductDetailsActivity.kt
@@ -2,27 +2,94 @@ package com.fakturku.aplikasi.ui.activity.productDetails
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.Menu
+import android.view.MenuItem
 import com.fakturku.aplikasi.R
 import com.fakturku.aplikasi.model.Product
+import com.fakturku.aplikasi.utils.MyCurrencyFormat
+import kotlinx.android.synthetic.main.activity_product_details.*
 
 class ProductDetailsActivity : AppCompatActivity(), ProductDetailsContract.View {
 
     private lateinit var presenter: ProductDetailsPresenter
+    private lateinit var product: Product
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
+        title = null
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
 
         //init presenter
         presenter = ProductDetailsPresenter(this@ProductDetailsActivity)
+
+        //Get Intent Data
+        if (intent.hasExtra(INTENT_DATA_PRODUCT)) {
+            product = intent.getParcelableExtra(INTENT_DATA_PRODUCT)
+            title = product.name
+            setProductData(product)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_product_details_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home ->{
+                onBackPressed()
+                true
+            }
+
+            R.id.productDetailsMenuUpdate->{
+                presenter.update(product)
+                return true
+            }
+
+            R.id.productDetailsMenuDelete->{
+                val strMsg = "Anda yakin akan menghapus ${product.name} dari database ?"
+                val yesMsg = "Ya, Hapus"
+                val noMsg = "Tidak"
+                AlertDialog.Builder(this@ProductDetailsActivity)
+                        .setMessage(strMsg)
+                        .setPositiveButton(yesMsg,{ _ , _ ->
+                            presenter.delete(product)
+                        })
+                        .setNegativeButton(noMsg, null)
+                        .show()
+                return true
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     override fun setProductData(product: Product) {
+        productDetailsName.text = product.name
+        val buyPrice = product.buy_price
+        if (buyPrice != null){
+            productDetailsBuyPrice.text = MyCurrencyFormat.rupiah(buyPrice)
+        }
+        val sellPrice = product.sell_price
+        if (sellPrice != null){
+            productDetailsSellPrice.text = MyCurrencyFormat.rupiah(sellPrice)
+        }
+        productDetailsNotes.text = product.notes
     }
 
     override fun delete(product: Product) {
     }
 
     override fun update(product: Product) {
+    }
+
+    companion object {
+        const val INTENT_DATA_PRODUCT: String = "IntentDataProduct"
     }
 }
--- app/src/main/java/com/fakturku/aplikasi/ui/fragment/productFragment/ProductFragment.kt
+++ app/src/main/java/com/fakturku/aplikasi/ui/fragment/productFragment/ProductFragment.kt
@@ -1,5 +1,6 @@
 package com.fakturku.aplikasi.ui.fragment.productFragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.DefaultItemAnimator
@@ -9,10 +10,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.fakturku.aplikasi.R
 import com.fakturku.aplikasi.model.Product
 import com.fakturku.aplikasi.ui.activity.DashboardActivity
+import com.fakturku.aplikasi.ui.activity.productDetails.ProductDetailsActivity
 import com.fakturku.aplikasi.ui.adapter.ProductListAdapter
 import kotlinx.android.synthetic.main.fragment_product.*
 import java.util.ArrayList
@@ -122,15 +125,67 @@ class ProductFragment : Fragment(), ProductContract.View {
     }
 
     override fun showProductDetails(product: Product) {
+        val intentSupplierDetails = Intent(activity, ProductDetailsActivity::class.java)
+        intentSupplierDetails.putExtra(ProductDetailsActivity.INTENT_DATA_PRODUCT, product)
+        startActivityForResult(intentSupplierDetails, INTENT_PRODUCT_DETAILS_CODE)
     }
 
     override fun openAddProductPage() {
+//        val intentAddSupplier = Intent(activity, SupplierFormActivity::class.java)
+//        startActivityForResult(intentAddSupplier, INTENT_ADD_SUPPLIER_CODE)
     }
 
     override fun openUpdateProductPage(product: Product) {
+//        val intentUpdateSupplier = Intent(activity, SupplierFormActivity::class.java)
+//        intentUpdateSupplier.putExtra(SupplierFormActivity.INTENT_SUPPLIER_DATA, supplier)
+//        startActivityForResult(intentUpdateSupplier, INTENT_UPDATE_SUPPLIER_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode){
+            INTENT_PRODUCT_DETAILS_CODE->{
+                when(resultCode){
+                    INTENT_PRODUCT_DETAILS_UPDATE->{
+                        if (data != null) {
+                            val product: Product = data.getParcelableExtra(INTENT_PRODUCT_DETAILS_UPDATE_DATA)
+                            presenter.updateProduct(product)
+                        }
+                    }
+                    INTENT_PRODUCT_DETAILS_DELETE->{
+                        Toast.makeText(activity,"DATA DELETED",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            INTENT_ADD_PRODUCT_CODE->{
+                when(resultCode){
+                    INTENT_ADD_PRODUCT_SUCCESS->{
+                        Toast.makeText(activity,"DATA ADDED",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            INTENT_UPDATE_PRODUCT_CODE->{
+                when(resultCode){
+                    INTENT_UPDATE_PRODUCT_SUCCESS->{
+                        Toast.makeText(activity,"DATA UPDATED",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else->{ super.onActivityResult(requestCode, resultCode, data) }
+        }
+
     }
 
     companion object {
+        const val INTENT_PRODUCT_DETAILS_CODE = 20
+        const val INTENT_PRODUCT_DETAILS_UPDATE = 21
+        const val INTENT_PRODUCT_DETAILS_DELETE = 22
+        const val INTENT_PRODUCT_DETAILS_UPDATE_DATA: String = "IntentProductUpdateData"
+
+        const val INTENT_ADD_PRODUCT_CODE = 30
+        const val INTENT_ADD_PRODUCT_SUCCESS = 31
+
+        const val INTENT_UPDATE_PRODUCT_CODE = 40
+        const val INTENT_UPDATE_PRODUCT_SUCCESS = 41
 
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
