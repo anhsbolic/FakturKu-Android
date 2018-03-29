87146bb374b4db24126828df4fb3ab139d77aafb
unknown <anhsbolic@gmail.com>
2018-03-29 16:51:43 +0700

    [ProductForm] layout update

--- app/src/main/java/com/fakturku/aplikasi/ui/activity/productForm/ProductFormActivity.kt
+++ app/src/main/java/com/fakturku/aplikasi/ui/activity/productForm/ProductFormActivity.kt
@@ -3,6 +3,7 @@ package com.fakturku.aplikasi.ui.activity.productForm
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.fakturku.aplikasi.R
+import com.fakturku.aplikasi.model.Product
 
 class ProductFormActivity : AppCompatActivity(), ProductFormContract.View {
 
@@ -15,4 +16,17 @@ class ProductFormActivity : AppCompatActivity(), ProductFormContract.View {
         //Init Presenter
         presenter = ProductFormPresenter(this@ProductFormActivity)
     }
+
+    override fun setUpdateMode(product: Product) {
+    }
+
+    override fun showErrorInput(isNameValid: Boolean) {
+    }
+
+    override fun showAddProductSuccess(product: Product) {
+    }
+
+    override fun showUpdateProductSuccess(product: Product) {
+    }
+
 }
