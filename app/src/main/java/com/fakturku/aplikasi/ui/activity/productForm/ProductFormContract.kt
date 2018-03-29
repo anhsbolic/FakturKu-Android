fc67b81918e547fec2f95072330c17eabda8df96
unknown <anhsbolic@gmail.com>
2018-03-29 16:36:38 +0700

    [ProductForm] set contract & presenter

--- app/src/main/java/com/fakturku/aplikasi/ui/activity/productForm/ProductFormContract.kt
+++ app/src/main/java/com/fakturku/aplikasi/ui/activity/productForm/ProductFormContract.kt
@@ -1,12 +1,32 @@
 package com.fakturku.aplikasi.ui.activity.productForm
 
+import com.fakturku.aplikasi.model.Product
+
 interface ProductFormContract{
 
     interface View{
 
+        fun setUpdateMode(product: Product)
+
+        fun showErrorInput(isNameValid: Boolean)
+
+        fun showAddProductSuccess(product: Product)
+
+        fun showUpdateProductSuccess(product: Product)
     }
 
     interface Presenter{
+
+        fun setUpdateMode(product: Product)
+
+        fun addCostumer(id: String?, name: String, buyPrice: Int?, sellPrice: Int?, notes: String?,
+                        createdDate: String?, updatedDate: String?, isEditMode: Boolean)
+
+        fun updateCostumer(id: String?, name: String, buyPrice: Int?, sellPrice: Int?, notes: String?,
+                           createdDate: String?, updatedDate: String?, isEditMode: Boolean)
+
+        fun validateInput(id: String?, name: String, buyPrice: Int?, sellPrice: Int?, notes: String?,
+                          createdDate: String?, updatedDate: String?, isEditMode: Boolean)
 
     }
 }
--- app/src/main/java/com/fakturku/aplikasi/ui/activity/productForm/ProductFormPresenter.kt
+++ app/src/main/java/com/fakturku/aplikasi/ui/activity/productForm/ProductFormPresenter.kt
@@ -1,6 +1,45 @@
 package com.fakturku.aplikasi.ui.activity.productForm
 
+import com.fakturku.aplikasi.model.Product
+
 class ProductFormPresenter(private val view: ProductFormContract.View)
     : ProductFormContract.Presenter{
+
+    override fun setUpdateMode(product: Product) {
+        view.setUpdateMode(product)
+    }
+
+    override fun addCostumer(id: String?, name: String, buyPrice: Int?, sellPrice: Int?, notes: String?,
+                             createdDate: String?, updatedDate: String?, isEditMode: Boolean) {
+        validateInput(id, name, buyPrice, sellPrice, notes, createdDate, updatedDate, isEditMode)
+    }
+
+    override fun updateCostumer(id: String?, name: String, buyPrice: Int?, sellPrice: Int?, notes: String?,
+                                createdDate: String?, updatedDate: String?, isEditMode: Boolean) {
+        validateInput(id, name, buyPrice, sellPrice, notes, createdDate, updatedDate, isEditMode)
+    }
+
+    override fun validateInput(id: String?, name: String, buyPrice: Int?, sellPrice: Int?, notes: String?,
+                               createdDate: String?, updatedDate: String?, isEditMode: Boolean) {
+
+        var isNameValidate = false
+
+        if (name.isNotEmpty()){
+            isNameValidate = true
+        }
+
+
+        if (isNameValidate ) {
+            val product = Product(id, name, buyPrice, sellPrice, notes, createdDate, updatedDate)
+
+            if (!isEditMode){
+                view.showAddProductSuccess(product)
+            } else {
+                view.showUpdateProductSuccess(product)
+            }
+        } else {
+            view.showErrorInput(isNameValidate)
+        }
+    }
 
 }
