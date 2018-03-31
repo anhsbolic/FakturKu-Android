61cdf65e09a06e551958f6b7534010f9527e4afa
unknown <anhsbolic@gmail.com>
2018-03-31 17:09:59 +0700

    add ripple, navigation to SettingInvoice

--- app/src/main/java/com/fakturku/aplikasi/ui/fragment/settingsFragment/MySettingsFragment.kt
+++ app/src/main/java/com/fakturku/aplikasi/ui/fragment/settingsFragment/MySettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.fakturku.aplikasi.ui.fragment.settingsFragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 
 import com.fakturku.aplikasi.R
+import com.fakturku.aplikasi.ui.activity.settingInvoice.SettingInvoiceActivity
 import kotlinx.android.synthetic.main.fragment_settings.*
 
 class MySettingsFragment : Fragment(), MySettingsContract.View {
@@ -36,10 +38,28 @@ class MySettingsFragment : Fragment(), MySettingsContract.View {
     }
 
     override fun showInvoiceSettings() {
-        Toast.makeText(activity, "SET FAKTUR", Toast.LENGTH_SHORT).show()
+        val intentSetInvoice = Intent(activity, SettingInvoiceActivity::class.java)
+        startActivityForResult(intentSetInvoice, INTENT_SET_INVOICE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode){
+            INTENT_SET_INVOICE_CODE->{
+                when(resultCode){
+                    INTENT_SET_INVOICE_SUCCESS->{
+                        Toast.makeText(activity,"INVOICE SETTINGS UPDATED",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else->{ super.onActivityResult(requestCode, resultCode, data) }
+        }
+
     }
 
     companion object {
+        const val INTENT_SET_INVOICE_CODE = 20
+        const val INTENT_SET_INVOICE_SUCCESS = 21
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
                 MySettingsFragment().apply {
