274d39d0931f11e9a28ad950bf261a6f99dd4207
unknown <anhsbolic@gmail.com>
2018-04-09 14:16:35 +0700

    create invoiceform

---
+++ app/src/main/java/com/fakturku/aplikasi/ui/activity/invoiceForm/InvoiceFormActivity.kt
@@ -0,0 +1,13 @@
+package com.fakturku.aplikasi.ui.activity.invoiceForm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fakturku.aplikasi.R
+
+class InvoiceFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invoice_form)
+    }
+}
