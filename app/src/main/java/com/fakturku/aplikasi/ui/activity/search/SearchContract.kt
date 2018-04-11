34819c0ba52089f574cccbc8b6820bd9d81ab23b
unknown <anhsbolic@gmail.com>
2018-04-11 15:58:43 +0700

    Create Search Page : Activity, layout, contract, presenter

---
+++ app/src/main/java/com/fakturku/aplikasi/ui/activity/search/SearchContract.kt
@@ -0,0 +1,12 @@
+package com.fakturku.aplikasi.ui.activity.search
+
+interface SearchContract{
+
+    interface View{
+
+    }
+
+    interface Presenter{
+
+    }
+}
---
+++ app/src/main/java/com/fakturku/aplikasi/ui/activity/search/SearchPresenter.kt
@@ -0,0 +1,6 @@
+package com.fakturku.aplikasi.ui.activity.search
+
+class SearchPresenter(private val view: SearchContract.View)
+    : SearchContract.Presenter{
+
+}
