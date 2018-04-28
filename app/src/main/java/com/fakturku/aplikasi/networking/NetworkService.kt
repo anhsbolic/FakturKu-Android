07d0851b5e5cdb15cfbe185c1fcb7cb4e8a5d2b5
unknown <anhsbolic@gmail.com>
2018-04-28 16:43:00 +0700

    add new product to db

--- app/src/main/java/com/fakturku/aplikasi/networking/NetworkService.kt
+++ app/src/main/java/com/fakturku/aplikasi/networking/NetworkService.kt
@@ -1,12 +1,21 @@
 package com.fakturku.aplikasi.networking
 
+import com.fakturku.aplikasi.model.Product
 import com.fakturku.aplikasi.model.ProductList
 import io.reactivex.Observable
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ApiService{
 
     @GET("product")
     fun getProductList(@Query("page") page: Int): Observable<ProductList>
+
+    @FormUrlEncoded
+    @POST("product")
+    fun saveProduct(@Field("name") name: String,
+                    @Field("purchase_price") purchasePrice: Int,
+                    @Field("sell_price") sellPrice: Int,
+                    @Field("info") info: String): Observable<Product>
 }
+
+//TODO : change int to long
--- app/src/main/java/com/fakturku/aplikasi/ui/activity/productForm/ProductFormPresenter.kt
+++ app/src/main/java/com/fakturku/aplikasi/ui/activity/productForm/ProductFormPresenter.kt
@@ -1,9 +1,32 @@
 package com.fakturku.aplikasi.ui.activity.productForm
 
+import android.util.Log
 import com.fakturku.aplikasi.model.Product
+import com.fakturku.aplikasi.networking.ApiService
+import com.fakturku.aplikasi.ui.fragment.productFragment.ProductPresenter
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ProductFormPresenter(private val view: ProductFormContract.View)
     : ProductFormContract.Presenter{
+
+    //initialized gson
+    private val gsonBuilder: Gson = GsonBuilder().create()
+
+    //initialized retrofit
+    private val retrofit: Retrofit = Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gsonBuilder))
+            .baseUrl(ProductPresenter.BASE_URL_DEV)
+            .build()
+
+    private val apiService: ApiService = retrofit.create(
+            ApiService::class.java)
 
     override fun setUpdateMode(product: Product) {
         view.setUpdateMode(product)
@@ -32,11 +55,20 @@ class ProductFormPresenter(private val view: ProductFormContract.View)
         if (isNameValidate ) {
             val product = Product(id, name, buyPrice, sellPrice, notes, createdDate, updatedDate)
 
-            if (!isEditMode){
-                view.showAddProductSuccess(product)
-            } else {
-                view.showUpdateProductSuccess(product)
-            }
+            apiService.saveProduct(product.name!!, product.purchase_price!!, product.sell_price!!, product.info!!)
+                    .subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            {savedProduct->
+                                if (!isEditMode){
+                                    view.showAddProductSuccess(savedProduct)
+                                } else {
+                                    view.showUpdateProductSuccess(savedProduct)
+                                }
+                            },
+                            {error->
+                                Log.e("Error", error.message)
+                            })
         } else {
             view.showErrorInput(isNameValidate)
         }
